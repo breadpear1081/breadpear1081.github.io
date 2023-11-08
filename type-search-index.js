076175a5ccedf212acbb746a8025ba31d4eb17bf
breadpear1081 <150292496+breadpear1081@users.noreply.github.com>
2023-11-08 14:03:22 -0600

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Session"},{"p":"<Unnamed>","l":"Student"},{"p":"<Unnamed>","l":"Tutor"}];updateSearchResults();
